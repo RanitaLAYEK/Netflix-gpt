4b03067fd331817765c2627adb9c2c5731e7c167
ranita <RanitaLAYEK@users.noreply.github.com>
2026-02-04 22:55:06 +0530

    chore: savepoint

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { LOGO } from "../utils/constant";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={LOGO} alt="netflix-img" />
+    </div>
+  );
+};
+
+export default Header;
